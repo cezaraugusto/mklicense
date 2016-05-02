e1c9457ac1a6c57dff241e39e9f6a54434ebaa92
Cezar Augusto <augusto01@live.com>
2016-05-02 01:06:56 -0300

    Update output text

--- mklicense.js
+++ mklicense.js
@@ -2,6 +2,7 @@
 'use strict'
 
 var fs = require('fs')
+var path = require('path')
 var inquirer = require('inquirer')
 var jsdom = require('node-jsdom')
 var username = require('git-user-name')
@@ -27,7 +28,7 @@ inquirer.prompt([
   {
     type: 'input',
     name: 'license_year',
-    message: 'The program license begins in:',
+    message: 'The project\'s license begins in:',
     default: function () {
       var getTheDate = new Date()
       var currentYear = getTheDate.getFullYear()
@@ -43,7 +44,7 @@ inquirer.prompt([
   {
     type: 'input',
     name: 'author_name',
-    message: 'What\'s the program author full name?',
+    message: 'The project\'s author full name:',
     default: function () {
       return username()
     },
@@ -57,9 +58,9 @@ inquirer.prompt([
   {
     type: 'input',
     name: 'project_description',
-    message: 'Give the program\'s name and a brief idea of what it does (one line).',
+    message: 'Give the project\'s name and a brief idea of what it does (one line):\n',
     default: function () {
-      return 'i.e. Mkdocs. A CLI tool to generate Licenses. Available on NPM.'
+      return 'mkdocs. A CLI tool that generates your next project\'s License. Available on NPM.'
     },
     when: function (answers) {
       var licenseOption = answers.licenseList
@@ -161,11 +162,12 @@ inquirer.prompt([
       })
     },
     done: function (errors, window) {
+      var whereIsMyLicense = path.resolve('.')
       if (errors) {
         console.log('Error while transfering the content for your file', errors)
       }
       loading.stop(true)
-      console.log('Done, license created ;)')
+      console.log('Done. license created!\nYour license is under: ' + whereIsMyLicense + '\nLawyers are now happy.')
     }
   })
 })
